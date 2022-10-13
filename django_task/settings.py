4d366d6d5300cef14885500fffd51361612fb395
albertoviciute <albertoviciutee@gmail.com>
2022-10-13 21:21:34 +0300

    password reset

--- django_task/settings.py
+++ django_task/settings.py
@@ -133,3 +133,10 @@ MEDIA_URL = 'media/'
 LOGIN_URL = '/registration/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'albertoviciutee@gmail.com'
+EMAIL_HOST_PASSWORD = 'VerySecret'
